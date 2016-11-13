1688f4ae68d07ef4889f2641b362640672fd4ac9
phibedy <philipp.schmette@gmx.de>
2016-11-14 00:02:21 +0100

    try and error

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ class ExampleConan(ConanFile):
     settings = None
     exports = "README.md"
     #conan libs you would like to use
-    requires = "lms/2.0@lms/stable"#,"cereal/1.2-0@lms/stable","gtest/1.8.0@lms/stable"
+    requires = "lms/2.0@lms/stable","cereal/1.2-0@lms/stable","gtest/1.8.0@lms/stable","Eigen3/3.2.8@bilke/stable"
 
     def imports(self):
         self.copy("*.so",dst=".")
